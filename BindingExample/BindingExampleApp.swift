e183ca9face4bc40eb8764203994fbf750d27b9d
katafuchix <katafuchix@gmail.com>
2023-10-18 15:00:13 +0900

    Initial Commit

---
+++ BindingExample/BindingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  BindingExampleApp.swift
+//  BindingExample
+//
+//  Created by cano on 2023/10/18.
+//
+
+import SwiftUI
+
+@main
+struct BindingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BindingExample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BindingExample
+//
+//  Created by cano on 2023/10/18.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
